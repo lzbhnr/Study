d91236deaad22e21ae88f8f0195f79a74fbee481
lizongbao <lzbsdust@163.com>
2024-09-30 00:30:53 +0800

    update

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,37 +4,48 @@ public class Test {
 
     public static void main(String[] args) {
 
+        Test t = new Test();
+        Thread tc = new Thread(t::printC);
+        Thread tb = new Thread(() -> t.printB(tc));
+        Thread ta = new Thread(() -> t.printA(tb));
+
+
+        ta.start();
+        tb.start();
+        tc.start();
     }
 
-    private void printA(Thread thread) {
+    private void printC() {
+        try {
+            Thread.sleep(5L);
+            LockSupport.park();
+            System.out.println("C");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void printA(Thread thread) {
 
         try {
-            Thread.sleep(10L);
-            LockSupport.park();
+            Thread.sleep(20L);
             System.out.println("A");
+            LockSupport.unpark(thread);
         } catch (InterruptedException e) {
             throw new RuntimeException();
         }
     }
 
-    private void printB(Thread thread) {
+    public static void printB(Thread thread) {
         try {
             Thread.sleep(10L);
             LockSupport.park();
             System.out.println("B");
+            LockSupport.unpark(thread);
         } catch (InterruptedException e) {
             throw new RuntimeException();
         }
     }
 
-    private void printC(Thread thread) {
-
-        try {
-            Thread.sleep(10L);
-            LockSupport.park();
-            System.out.println("C");
-        } catch (InterruptedException e) {
-            throw new RuntimeException();
-        }
-    }
 }
