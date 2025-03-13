fc9b74d5db2b0794d6672413e0131c557aa05fd4
lizongbao <lzbsdust@163.com>
2025-03-13 16:39:19 +0800

    update

---
+++ src/main/java/Thread/CCode.java
@@ -0,0 +1,14 @@
+package Thread;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CCode {
+
+    public static void main(String[] args) {
+
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+
+        concurrentHashMap.put("","");
+    }
+}
