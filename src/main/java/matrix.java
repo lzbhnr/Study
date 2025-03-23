3023b2fdf1cc0fbcc90349d4d8e243d1c9148ebc
lizongbao <lzbsdust@163.com>
2025-03-23 13:10:07 +0800

    update

--- src/main/java/matrix.java
+++ src/main/java/matrix.java
@@ -25,6 +25,113 @@ public class matrix {
 
 
     public static void main(String[] args) {
+        int [] nums = {12,11,10,10,10,9,9,2};
+        int result = findTheFirst(nums,1,3,nums[5]);
+        System.out.println(result);
+        System.out.println(nums[result]);
 
+
+        //int resu = findLastGreater(nums,9);
+        //System.out.println(resu);
     }
+
+
+
+    public int findTheFirst22(int [] arr,int lf ,int rt ,int target){
+
+        int left = lf;
+        int right = rt;
+
+        int pos = lf; // lf 的位置肯定是最后的结。
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= target) {
+                pos = mid;       // 记录可能的位置
+                right = mid - 1; // 继续向左查找更小的索引
+            } else {
+                left = mid + 1;  // 向右查找更大的值
+                pos = mid-1;
+            }
+        }
+
+        // 根据pos判断结果
+        if (pos > 0) {
+            return pos - 1; // 最后一个大于target的元素
+        } else {
+            return -1; // 无解
+        }
+    }
+
+
+
+
+
+
+    public static int findTheFirst(int [] nums,int lf ,int rt ,int target){
+        int res = lf;
+        while(lf<=rt){
+            // 左边的边界，小于右边的边界
+
+            int mid = lf+(rt-lf)/2;
+            // 中间值
+
+
+            if(nums[mid]==target){
+                // 如果中间值 rt = mid-1;
+                rt = mid-1;
+            } else if (nums[mid] <target) {
+                rt = mid+1;
+            } else {
+               lf = mid+1;
+               if (lf == rt - 1) {
+                    res = nums[rt] > nums[i - 1] ? rigth : left;
+                    break;
+               }
+            }
+
+        }
+        return res;
+    }
+
+    public static int findTheFirst1(int [] nums,int lf ,int rt ,int target){
+
+
+        while(lf<=rt){
+            int mid = lf+(rt-lf)/2;
+            if(nums[mid]>target){
+                rt = mid-1;
+            }
+            else{
+                lf = mid+1;
+            }
+
+        }
+        return lf;
+    }
+
+    public static int findLastGreater(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int pos = 0; // 初始化位置为数组末尾之后，假设所有元素均大于target
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= target) {
+                pos = mid;       // 记录可能的位置
+                right = mid - 1; // 继续向左查找更小的索引
+            } else {
+                left = mid + 1;  // 向右查找更大的值
+                pos = mid-1;
+            }
+        }
+
+        // 根据pos判断结果
+        if (pos > 0) {
+            return pos - 1; // 最后一个大于target的元素
+        } else {
+            return -1; // 无解
+        }
+    }
+
 }
